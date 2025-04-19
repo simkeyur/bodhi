bff0ebd84d5f2d05661a8f375335e43708484ca8
Keyur Patel <simkeyur@gmail.com>
2025-04-18 23:11:46 -0500

    firebase setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { https } from 'firebase-functions';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import express from 'express';
+
+const server = express();
+
+export const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  return app.init();
+};
+
+createNestServer(server)
+    .then(v => console.log('Nest Ready'))
+    .catch(err => console.error('Nest broken', err));
+
+export const api = https.onRequest(server);
