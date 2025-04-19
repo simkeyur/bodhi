ab067cd1ac0c1c0e378eabd2d8f0a59ed030d7d8
Keyur Patel <simkeyur@gmail.com>
2025-04-19 01:23:44 -0500

    removing firebase stuff

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import express from 'express';
-import * as functions from 'firebase-functions';
-
-const expressApp = express();
+import { env } from '~configs/env.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-  app.enableCors(); // Enable CORS for cross-origin requests
-  await app.init();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-
-bootstrap()
-  .then(() => console.log('NestJS app initialized'))
-  .catch(err => console.error('NestJS app initialization failed', err));
-
-// A simple test endpoint
-expressApp.post('/api/gemini/text', (req, res) => {
-  res.json({ message: 'Hello from API!' });
-});
-
-// Optional: For debugging, also respond to GET
-expressApp.get('/api/gemini/text', (req, res) => {
-  res.send('GET: Hello from API!');
-});
-
-export const api = functions.https.onRequest(expressApp);
+bootstrap();
