18cde0b9bb4405982299ccb309edb0ab4b474ee7
Keyur Patel <simkeyur@gmail.com>
2025-04-18 23:53:18 -0500

    switched to functions

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ import { AppModule } from './app.module';
 import express from 'express';
 import * as functions from 'firebase-functions';
 
-const server = express();
+const expressApp = express();
 
-const createNestApp = async (expressInstance: express.Express) => {
+async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(expressInstance),
+    new ExpressAdapter(expressApp),
   );
   app.enableCors();
   await app.init();
   return app;
-};
+}
 
-export const api = functions.https.onRequest(async (request, response) => {
-  const app = await createNestApp(server);
-  const expressInstance = app.getHttpAdapter().getInstance();
-  return expressInstance(request, response);
-});
+bootstrap()
+  .then(() => console.log('NestJS app initialized'))
+  .catch((err) => console.error('NestJS app initialization failed', err));
+
+export const api = functions.https.onRequest(expressApp);
